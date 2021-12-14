2cd51ed93afa8c3d05b563ab117efaf4e71c3b7d
THOMAS BAGNATO <thomas.bagnato@atos.net>
2021-12-14 16:22:48 +0100

    [SPOILER] Day 14 solution 1 (OK) + 2 (Highly inneficient)

--- AdventOfCode/2021/Days/Day14.cs
+++ AdventOfCode/2021/Days/Day14.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Business
 {
@@ -7,15 +11,50 @@ namespace AdventOfCode.Business
     {
         private static class Day14
         {
-            public static string Test1()
-            {
+            public static string Test1() => Test(10);
 
-                return string.Empty;
-            }
-            public static string Test2()
-            {
+            public static string Test2() => Test(40);
 
-                return string.Empty;
+            private static string Test(int steps)
+            {
+                var input = File.ReadAllLines($"{INPUT_PATH}day14.txt");
+                var polymer = new StringBuilder(input.First());
+                var rules = input.Where(l => l.Contains("-")).Select(l =>
+                {
+                    var a = l.Split(new string[] { " -> " }, StringSplitOptions.None);
+                    return (a[0], a[1]);
+                }).OrderBy(h => h.Item1);
+
+                var doRules = new List<(Group, string)>();
+
+                for (int i = 0; i < steps; i++)
+                {
+                    foreach (var rule in rules)
+                    {
+                        var matches = Regex.Matches(polymer.ToString(), $"(?={rule.Item1})");
+                        foreach (Match match in matches)
+                        {
+                            foreach (var g in match.Groups.Values)
+                            {
+                                doRules.Add((g, rule.Item2));
+                            }
+                        }
+                    }
+
+                    var orderedRules = doRules.OrderBy(g => g.Item1.Index);
+
+                    var j = 0;
+                    foreach (var group in orderedRules)
+                    {
+                        polymer.Insert(group.Item1.Index + 1 + j, group.Item2);
+                        j++;
+                    }
+
+                    doRules.Clear();
+                }
+                var ret = polymer.ToString().GroupBy(c => c).OrderByDescending(g => g.LongCount());
+
+                return (ret.First().LongCount() - ret.Last().LongCount()).ToString();
             }
         }
     }
---
+++ AdventOfCode/2021/Days/Day22.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Business
+{
+    internal sealed partial class AoC21
+    {
+        private static class Day22
+        {
+            public static string Test1()
+            {
+
+                return string.Empty;
+            }
+            public static string Test2()
+            {
+
+                return string.Empty;
+            }
+        }
+    }
+}
