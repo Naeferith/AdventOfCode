80496a441e41fead2abac05fe9d90beb94941904
Naeferith <toto2020@hotmail.fr>
2022-12-04 10:40:58 +0100

    2022 - 04

---
+++ AdventOfCode/V2022/Days/Day4.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode.Core.AoC;
+using System;
+using System.Linq;
+
+namespace AdventOfCode.V2022.Days
+{
+    internal class Day4 : IDay
+    {
+        public string PuzzleName => "Camp Cleanup";
+
+        public string Solution1(string[] lines)
+        {
+            return Solution(lines, p => p.Item2 - p.Item1, SetIncluded);
+        }
+
+        public string Solution2(string[] lines)
+        {
+            return Solution(lines, p => p.Item1, Overlaps);
+        }
+
+        private static string Solution(string[] lines,
+            Func<(int, int), int> order,
+            Func<(int, int), (int, int), bool> predicate)
+        {
+            return lines
+                .Select(l =>
+                    l.Split(',')
+                    .Select(r => r.Split('-').Select(int.Parse))
+                    .Select(i => (i.First(), i.Last()))
+                    .OrderBy(order)
+                )
+                .Count(p => predicate(p.First(), p.Last()))
+                .ToString();
+        }
+
+        private static bool SetIncluded((int, int) min, (int, int) max)
+        {
+            return max.Item1 <= min.Item1 && max.Item2 >= min.Item2;
+        }
+
+        private static bool Overlaps((int, int) min, (int, int) max)
+        {
+            return min.Item2 >= max.Item1;
+        }
+    }
+}
