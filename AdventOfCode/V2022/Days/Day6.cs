ae34a8d6adcf6d067ee23b07bf4b603613da53d9
Naeferith <toto2020@hotmail.fr>
2022-12-06 09:04:02 +0100

    2022 - 06

---
+++ AdventOfCode/V2022/Days/Day6.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Core.AoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.V2022.Days
+{
+    internal class Day6 : IDay
+    {
+        public string PuzzleName => "Tuning Trouble";
+
+        public string Solution1(string[] lines)
+        {
+            return Solution(lines[0], 4);
+        }
+
+        public string Solution2(string[] lines)
+        {
+            return Solution(lines[0], 14);
+        }
+
+        private static string Solution(string data, int buffSize)
+        {
+            var buffer = new List<char>();
+            for (int i = 0; i < buffSize; i++) buffer.Add(data[i]);
+
+            for (int i = buffSize; i < data.Length; i++)
+            {
+                buffer.RemoveAt(0);
+                buffer.Add(data[i]);
+
+                if (buffer.Distinct().Count() == buffSize)
+                    return (i + 1).ToString();
+            }
+
+            return null;
+        }
+    }
+}
