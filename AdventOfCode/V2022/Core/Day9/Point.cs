7000830e7bce4373b62102ab756d5d2c06a2bfb0
Naeferith <toto2020@hotmail.fr>
2022-12-09 10:53:37 +0100

    2022 - 09

---
+++ AdventOfCode/V2022/Core/Day9/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdventOfCode.V2022.Core.Day9
+{
+    internal class Point : IEquatable<Point>
+    {
+        public static Point Zero => new();
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(Point p)
+        {
+            X = p.X;
+            Y = p.Y;
+        }
+
+        public bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+    }
+}
