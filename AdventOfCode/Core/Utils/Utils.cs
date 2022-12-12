bfb9e29684bd5e5c24f4db1a10b89a0c61f418d4
Naeferith <toto2020@hotmail.fr>
2022-12-12 18:07:31 +0100

    2022 - 11 (finally)

--- AdventOfCode/Core/Utils/Utils.cs
+++ AdventOfCode/Core/Utils/Utils.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace AdventOfCode.Core.Utils
 {
@@ -11,6 +13,27 @@ namespace AdventOfCode.Core.Utils
                 .Select((x, i) => new { Index = i, Value = x })
                 .GroupBy(x => x.Index / chunkSize)
                 .Select(x => x.Select(v => v.Value));
+        }
+
+        public static T Gcd<T>(T a, T b) where T : IBinaryInteger<T>
+        {
+            while (b != default)
+            {
+                T temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static T Lcm<T>(IEnumerable<T> numerics) where T : IBinaryInteger<T>
+        {
+            var res = (T)Convert.ChangeType(1, typeof(T));
+            foreach (var i in numerics)
+            {
+                res = res / Gcd(res, i) * i;
+            }
+            return res;
         }
     }
 }
---
+++ AdventOfCode/V2022/Core/Day11/Monkey.cs
@@ -0,0 +1,81 @@
+﻿using AdventOfCode.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.V2022.Core.Day11
+{
+    internal sealed class Monkey
+    {
+        public bool Reduce { get; }
+        public int InspectCount { get; private set; }
+        private int Divisor { get; set; }
+
+        private List<ulong> Items { get; set; }
+        private Func<ulong, ulong> WorryOperation { get; set; }
+        private Func<ulong, int> TestOperation { get; set; }
+
+        public Monkey(bool reduce)
+        {
+            Reduce = reduce;
+        }
+
+        private static Func<ulong, ulong> GetOperation(string op, ulong? rOp)
+        {
+            return op switch
+            {
+                "+" => (old) => old + (rOp ?? old),
+                "*" => (old) => old * (rOp ?? old),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        public void SetItems(IEnumerable<int> items)
+        {
+            Items = new(items.Select(i => (ulong)i));
+        }
+
+        public void SetOperation(string operation)
+        {
+            var rValue = operation.Split('=').Last().Trim();
+            var args = rValue.Split(' ');
+
+            ulong? rOp = null;
+
+            if (ulong.TryParse(args[2], out var rInt))
+                rOp = rInt;
+
+            WorryOperation = GetOperation(args[1], rOp);
+        }
+
+        public void SetTest(int denominator, int trueMonkey, int falseMonkey)
+        {
+            Divisor = denominator;
+            TestOperation = (level) => level % (ulong)Divisor == 0
+                ? trueMonkey
+                : falseMonkey;
+        }
+
+        public void Inspect(IList<Monkey> monkeys)
+        {
+            var lcm = Utils.Lcm(monkeys.Select(m => m.Divisor));
+
+            InspectCount += Items.Count;
+
+            foreach (var item in Items)
+            {
+                var newWorry = WorryOperation(item);
+
+                if (Reduce)
+                    newWorry /= 3;
+
+                newWorry %= (ulong)lcm;
+
+                var index = TestOperation(newWorry);
+                monkeys[index].Items.Add(newWorry);
+            }
+
+            Items.Clear();
+        }
+    }
+}
