2bdb78ee41f347e47c5335301f69c83feca29a6c
Naeferith <toto2020@hotmail.fr>
2023-12-02 11:58:44 +0100

    Cleanup

--- AdventOfCode.V2023/Days/Day2/Day2.cs
+++ AdventOfCode.V2023/Days/Day2/Day2.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode.V2023.Days.Day2
         public string Solution1(string[] lines)
         {
             return lines
-                .Select((l, i) => new { Id = i + 1, Sets = l.Split(": ")[1].Split("; ").Select(s => new Set(s)) })
+                .Select((l, i) => new { Id = i + 1, Sets = ParseSets(l) })
                 .Where(a => a.Sets.All(s => s.Red <= 12 && s.Blue <= 14 && s.Green <= 13))
                 .Select(a => a.Id)
                 .Sum()
@@ -21,11 +21,16 @@ namespace AdventOfCode.V2023.Days.Day2
         public string Solution2(string[] lines)
         {
             return lines
-                .Select(l => l.Split(": ")[1].Split("; ").Select(s => new Set(s)))
+                .Select(ParseSets)
                 .Select(s => new Set(s.Max(s => s.Blue), s.Max(s => s.Red), s.Max(s => s.Green)))
                 .Select(s => s.Red * s.Blue * s.Green)
                 .Sum()
                 .ToString();
+        }
+
+        private static IEnumerable<Set> ParseSets(string l)
+        {
+            return l.Split(": ")[1].Split("; ").Select(s => new Set(s));
         }
     }
 }
